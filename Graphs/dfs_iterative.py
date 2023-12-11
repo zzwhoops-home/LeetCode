3c85d0fe311a3929c41a5540e2f61f6661cc165a
zzwhoops-home <zd@5rock.org>
2023-12-11 18:34:12 -0500

    dfs iterative approaches

---
+++ Graphs/dfs_iterative.py
@@ -0,0 +1,42 @@
+from collections import deque
+
+def depthFirstIterativePreOrder(graph, src):
+    stack = deque([src])
+    visited = set()
+
+    while len(stack) > 0:
+        cur = stack.pop()
+        if cur not in visited:
+            print(cur)
+            visited.add(cur)
+
+        for i in range(len(graph[cur]) - 1, -1, -1):
+            stack.append(graph[cur][i])
+
+def depthFirstIterativePostOrder(graph, src):
+    stack = deque([src])
+    visited = set()
+
+    while len(stack) > 0:
+        cur = stack.pop()
+        if cur not in visited:
+            print(cur)
+            visited.add(cur)
+
+        for vertex in graph[cur]:
+            stack.append(vertex)
+
+graph = {
+    'a': ['b', 'c'],
+    'b': ['d'],
+    'c': ['e'],
+    'd': ['f'],
+    'e': [],
+    'f': []
+}
+
+if __name__==("__main__"):
+    depthFirstIterativePreOrder(graph, 'a')
+    depthFirstIterativePostOrder(graph, 'a')
+    print()
+    depthFirstIterativePostOrder(graph, 'd')
